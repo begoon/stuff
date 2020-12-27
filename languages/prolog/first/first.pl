5acfdd819a2c8fd19ace4a0c2ca0e26c593bd3a3
Alexander Demin <alexander@demin.ws>
2020-12-27 09:12:30 +0000

    Update after watch Prolog crash course

--- languages/prolog/first/first.pl
+++ languages/prolog/first/first.pl
@@ -1,8 +1,25 @@
-parent(sasha, tessa).
-parent(sasha, danila).
+parent(alexander, tessa).
+parent(alexander, danyil).
 parent(olga, tessa).
-parent(olga, danila).
+parent(olga, danyil).
 
 sibling(A, B) :- \+(A = B), parent(Z, A), parent(Z, B).
 couple(A, B) :- \+(A = B), parent(A, Z), parent(B, Z).
 
+family(F, M, []) :-
+    couple(F, M).
+
+family(F, M, [C|T]) :-
+    couple(F, M),
+    parent(F, C),
+    parent(M, C),
+	family(F, M, T).
+
+child(C) :-
+    parent(_, C).
+
+children([]).
+
+children([H|T]) :-
+    child(H),
+    children(T).
