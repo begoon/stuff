5ad43becda6369f6dcd83b89aa7a99ada3cc5967
Alexander Demin <alexander@demin.ws>
2020-12-27 10:36:25 +0000

    Create call-cc.scm

---
+++ languages/scheme/call-cc.scm
@@ -0,0 +1,22 @@
+(let (
+  (value (call/cc
+    (lambda (the-continuation) 
+      (display "this should be printed\n")
+      (the-continuation 100)
+      (display "this should NOT be printed\n")))))
+  (display (format "value=~s\n" value))
+
+(display "\n")
+
+(let (
+  (value (call/cc
+      (lambda (the-continuation) 
+        the-continuation))))
+  (if (procedure? value)
+    (begin 
+      (display (format "1. continuation~%"))
+      (value "value is now a string"))
+    (begin 
+      (display (format "2. NOT continuation\nvalue=~s\n" value))
+  )
+)
